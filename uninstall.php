408010a504862b263717d0bb52b2f90210d1ccb7
meitar <meitar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-01-11 22:56:37 +0000

    Also delete the `tumblr_reblog_key` when "leaving no trace."
    git-svn-id: https://plugins.svn.wordpress.org/tumblr-crosspostr/trunk@1572907 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -13,6 +13,7 @@ $options = get_option('tumblr_crosspostr_settings');
 if ($options['leave_no_trace']) {
     delete_post_meta_by_key('tumblr_post_id');
     delete_post_meta_by_key('tumblr_base_hostname');
+    delete_post_meta_by_key('tumblr_reblog_key');
 }
 
 // Delete all temporary post metadata values.
